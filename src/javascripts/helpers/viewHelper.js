a5fca779e1bfc89d90be3d9027cb46842987b675
Summer Duke <esrduke@gmail.com>
2020-10-15 19:26:16 -0500

    create food module

--- src/javascripts/helpers/viewHelper.js
+++ src/javascripts/helpers/viewHelper.js
@@ -2,6 +2,8 @@ import foodView from '../components/views/foodView';
 import showsView from '../components/views/showsView';
 import staffView from '../components/views/staffView';
 import souvenirsView from '../components/views/souvenirsView';
+import addShowsView from '../components/views/addShowsView';
+import addFoodView from '../components/views/addFoodView';
 
 const viewHelper = (id) => {
   $('#app').html('');
@@ -14,6 +16,10 @@ const viewHelper = (id) => {
       return showsView.showsView();
     case 'staff-link':
       return staffView.staffView();
+    case 'add-show-link':
+      return addShowsView.addShowsView();
+    case 'add-food-link':
+      return addFoodView.addFoodView();
     default:
       return console.warn('nothing clicked');
   }
@@ -24,6 +30,12 @@ const viewListener = (view) => {
   $('body').on('click', 'li.nav-item', (e) => {
     e.stopImmediatePropagation();
     viewHelper(e.currentTarget.id);
+  });
+  $('body').on('click', '.add-show', (e) => {
+    viewHelper('add-show-link', e.currentTarget.id);
+  });
+  $('body').on('click', '.add-food', (e) => {
+    viewHelper('add-food-link', e.currentTarget.id);
   });
 };
 
