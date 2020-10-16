7447bdf88dad650531bf6164c7b8f79998f88282
Jonathon Joyner <jonathonandon@gmail.com>
2020-10-15 19:14:13 -0500

    took out console log

--- src/javascripts/components/views/addStaffView.js
+++ src/javascripts/components/views/addStaffView.js
@@ -3,7 +3,6 @@ import form from '../forms/staffForm';
 const addStaffView = () => {
   $('#app').html('<div id="staff-form">Put staff form here</div>');
   form.staffForm();
-  console.warn('cool');
 };
 
 export default { addStaffView };
