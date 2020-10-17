44bdca3190a7e84e31cb2b9d727d4309868e14bf
Sam Mudick <smudick@gmail.com>
2020-10-16 19:43:25 -0500

    save

--- src/javascripts/components/views/souvenirsView.js
+++ src/javascripts/components/views/souvenirsView.js
@@ -2,6 +2,7 @@ import souvenirsData from '../../helpers/data/souvenirsData';
 import card from '../cards/souvenirsCard';
 
 const souvenirsView = () => {
+  $('#image-header').html('<div> <img src="https://lionhearthats.files.wordpress.com/2015/07/023.jpg" height="345"> </div>');
   souvenirsData.getAllSouvenirs().then((response) => {
     if (response.length) {
       response.forEach((souvenir) => {
