787c4220469a026aa663e184b358aa23664ee793
josephtmartin <goldglovejo13@gmail.com>
2020-10-19 21:38:01 -0500

    added delete events functionality

--- src/javascripts/components/cards/eventCard.js
+++ src/javascripts/components/cards/eventCard.js
@@ -1,5 +1,7 @@
+import eventData from '../../helpers/data/eventData';
+
 const buildEventCard = (eventObject) => {
-  const domString = `<div class="card m-2" id="${eventObject.firebaseKey}" style="width: 18rem;" id="${eventObject.firebaseKey}">
+  const domString = `<div class="card event-card m-2" id="${eventObject.firebaseKey}" style="width: 18rem;" id="${eventObject.firebaseKey}">
   <div class="card-body" id="${eventObject.firebaseKey}">
     <h5 class="card-title" id="${eventObject.firebaseKey}">${eventObject.name}</h5>
     </div>
@@ -9,7 +11,14 @@ const buildEventCard = (eventObject) => {
       </div>
       <p class="card-info" id="${eventObject.firebaseKey}">Date: ${eventObject.date}</p> 
     </div>
+    <button id="${eventObject.firebaseKey}" class="btn btn-outline delete-event icon-btn"><i id="event-icon" class="fas fa-times"></i></button>
   </div>`;
+  $('body').on('click', '.delete-event', (e) => {
+    e.stopImmediatePropagation();
+    const firebaseKey = e.currentTarget.id;
+    $(`.event-card#${firebaseKey}`).remove();
+    eventData.deleteEvent(firebaseKey);
+  });
 
   return domString;
 };
