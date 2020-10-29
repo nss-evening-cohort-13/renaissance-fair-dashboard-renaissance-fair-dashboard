ea53155b0dd30fc38af2dc20d833b98644d262ac
Chris LoJacono <clojacono92@gmail.com>
2020-10-29 13:03:30 -0500

    fixed small thing in modal title

--- src/javascripts/components/views/modal.js
+++ src/javascripts/components/views/modal.js
@@ -20,7 +20,7 @@ const foodModal = (eventFirebaseKey) => {
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
+          <h5 class="modal-title">Add Food To This Event</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
